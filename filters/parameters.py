9b51d7ad99e672eb55dacdc839c4238484669717
carlosgregoriorodriguez <cgr@sip.ucm.es>
2012-02-23 13:54:23 +0100

    Update filters/parameters.py

--- filters/parameters.py
+++ filters/parameters.py
@@ -1,3 +1,5 @@
 parameters = {}
+# Ok. 
 parameters['blur'] = {'ksize':(3,7,lambda x: 2*x+1)}
+# threshold1 find limit...
 parameters['Canny'] = {'threshold1':(200,600), 'threshold2':(80,600), 'apertureSize': (1,2,lambda x: 2*x + 3)}
