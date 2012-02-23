6c7dc23228bac01a221630a0a09cc13ffb206756
carlos <cgr@sip.ucm.es>
2012-02-23 18:26:55 +0100

    Canny test template

---
+++ filters/Canny.py
@@ -0,0 +1,16 @@
+from utils import *
+
+if __name__ == "__main__":
+    debug = False
+    camera =  cv2.VideoCapture(0)
+        
+    func_names = ['Canny']
+    
+    for name in func_names:
+        createTestFrame(name,parameters[name])
+    while 1:
+        img = getFrame(camera,grayscale=True)
+        cv2.imshow("original",img)
+        for name in func_names:
+            test(name,img,eval("cv2."+name),parameters[name])
+        InputKey(camera)        
