7189a7001bd9bbb1ec0e91fbf7b8b796a4c41034
carlos <cgr@sip.ucm.es>
2012-02-23 18:22:32 +0100

    template for fast opencv function study

--- filters/parameters.py
+++ filters/parameters.py
@@ -1,5 +1,14 @@
 parameters = {}
-# Ok. 
-parameters['blur'] = {'ksize':(3,7,lambda x: 2*x+1)}
-# threshold1 find limit...
+
+
+parameters['medianBlur'] = {'ksize':(3,7,lambda x: 2*x+1)}
+#Ok
+
+
 parameters['Canny'] = {'threshold1':(200,600), 'threshold2':(80,600), 'apertureSize': (1,2,lambda x: 2*x + 3)}
+# threshold1 find limit...
+
+
+parameters['erode'] = {'iterations':(1,7), 'kernel':(0,1,lambda x: None)} 
+
+parameters['dilate'] = {'iterations':(1,7), 'kernel':(0,1,lambda x: None)}
---
+++ filters/utils.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+#! /usr/bin/env python
+# opencv 2.3.1
+
+import sys
+import time
+import cv2.cv as cv #solo para algunas constantes
+import cv2
+import numpy as np
+from parameters import parameters
+
+def dummy(v):
+    global debug
+    if debug :
+        print "modified value:", v
+
+def createTestFrame(window_name, parameters):
+    cv2.namedWindow(window_name)
+    for k, v in parameters.iteritems():
+        if len(v) > 2:
+            cv2.createTrackbar(k+v[2].func_name,window_name,v[0],v[1],dummy)
+        else:
+            cv2.createTrackbar(k, window_name,v[0],v[1],dummy)
+
+def test(window_name, img_in, f, parameters, speed=True):      
+    func_param = {}
+    for k in iter(parameters):
+        if len(parameters[k]) > 2:
+            func_param[k] = parameters[k][2](cv2.getTrackbarPos(k+parameters[k][2].func_name,window_name))
+        else:
+            func_param[k] = cv2.getTrackbarPos(k,window_name) 
+    before = time.time()
+    img_out = apply(f,(img_in,),func_param)
+    if speed :
+        fps = 1/(time.time()-before)
+        cv2.putText(img=img_out, text=str(fps)+" fps", org=(50, 50), 
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1.0, 
+                    color=(0, 0, 0), thickness = 2, linetype=cv2.CV_AA)        
+    cv2.imshow(window_name, img_out)
+    return img_out
+
+def getFrame(camera, grayscale=False, flip=True):    
+    f,frame = camera.read()
+    while not f:
+        print "Lost frame... trying again"
+        f,frame = camera.read()
+    if flip:
+        frame = cv2.flip(frame, 1)
+    if grayscale:
+        return cv2.cvtColor(frame,cv.CV_RGB2GRAY)
+    else:
+        return frame
+
+def InputKey(camera): 
+    global debug
+    QUIT = "q" 
+    k = cv2.waitKey (10)
+    if (k != -1):
+        if (k in range(256)):          
+            step = 0.1
+            if (chr(k) == QUIT):
+                print "Quit"
+                exit()
+            elif (chr(k) == "1"):
+                cb = camera.get(cv.CV_CAP_PROP_BRIGHTNESS)
+                if debug :
+                    print "-- brightness: ", cb - step
+                camera.set(cv.CV_CAP_PROP_BRIGHTNESS, cb - step)
+            elif (chr(k) == "2"): 
+                cb = camera.get(cv.CV_CAP_PROP_BRIGHTNESS)
+                if debug :
+                    print "++ brightness ", cb + step
+                camera.set(cv.CV_CAP_PROP_BRIGHTNESS, cb + step)
+            elif debug:
+                print "key", chr(k), "value", k
+        else:
+            if debug :
+                print "key value not valid for chr()", k    
+            
+if __name__ == "__main__":
+    print "Sample code for using utils mini module"
+    debug = False
+    camera =  cv2.VideoCapture(0)
+        
+    func_names = ['erode','dilate','Canny']
+    
+    for name in func_names:
+        createTestFrame(name,parameters[name])
+    while 1:
+        img = getFrame(camera,grayscale=True)
+        cv2.imshow("original",img)
+        for name in func_names:
+            test(name,img,eval("cv2."+name),parameters[name])
+        InputKey(camera)        
