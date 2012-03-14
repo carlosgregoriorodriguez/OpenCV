4c4c0c7e48a8f5acf33b058401c83542e84659cc
Willie Monge <willie9monge@gmail.com>
2012-03-14 18:01:10 +0100

    unnecessary comments removed

--- analisys/DFT/dftbasic.py
+++ analisys/DFT/dftbasic.py
@@ -19,11 +19,9 @@ if __name__ == "__main__":
 
     auxF = cv2.dft(bwDFT,flags = cv.CV_DXT_INV_SCALE)
     auxC = cv2.dct(bwDCT,flags = cv.CV_DXT_INV_SCALE)
-    #resultC = np.zeros(bwx.shape)
 
     resultF = np.array(auxF, dtype = "uint8")
     resultC = np.array(auxC, dtype = "uint8")
-    #cv2.dct(bwDCT,resultC,cv2.DCT_INVERSE)
 
     cv2.imshow("ORIGINAL",bw)
     #cv2.imshow("FOURIER",bwDFT)
@@ -32,8 +30,6 @@ if __name__ == "__main__":
     cv2.imshow("FOURIER INV.",resultF)
     cv2.imshow("COSINE INV.",resultC)
 
-
-    #cv2.imshow("SUM",bwDCT + bwDFT)
 
     while True:
         
