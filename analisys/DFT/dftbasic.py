fa863495b1cb7a7cfd26cdb0f0defc9868b99c8b
Willie Monge <willie9monge@gmail.com>
2012-03-14 17:40:31 +0100

    Arreglado inversa de DFT y DCT

--- analisys/DFT/dftbasic.py
+++ analisys/DFT/dftbasic.py
@@ -4,6 +4,7 @@
 
 
 import cv2
+import cv2.cv as cv
 import numpy as np
 import sys
 
@@ -14,20 +15,22 @@ if __name__ == "__main__":
     bwx = np.array(bw,dtype = float)           #convert the array to floats
 
     bwDFT = cv2.dft(bwx)                       #APPLY DFT and DCT
-    #bwDCT = cv2.dct(bwx)
+    bwDCT = cv2.dct(bwx)
 
-    resultF = np.zeros(bwx.shape)              #Create dst pointers
+    auxF = cv2.dft(bwDFT,flags = cv.CV_DXT_INV_SCALE)
+    auxC = cv2.dct(bwDCT,flags = cv.CV_DXT_INV_SCALE)
     #resultC = np.zeros(bwx.shape)
 
-    cv2.dft(bwDFT,resultF,cv2.DFT_INVERSE)
+    resultF = np.array(auxF, dtype = "uint8")
+    resultC = np.array(auxC, dtype = "uint8")
     #cv2.dct(bwDCT,resultC,cv2.DCT_INVERSE)
 
     cv2.imshow("ORIGINAL",bw)
-    cv2.imshow("FOURIER",bwDFT)
+    #cv2.imshow("FOURIER",bwDFT)
     #cv2.imshow("COSINE",bwDCT)
 
     cv2.imshow("FOURIER INV.",resultF)
-    #cv2.imshow("COSINE INV.",resultC)
+    cv2.imshow("COSINE INV.",resultC)
 
 
     #cv2.imshow("SUM",bwDCT + bwDFT)
