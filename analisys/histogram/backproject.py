667e276d796e30338c29839ea91a879523524f89
Oleg Zaytsev <lambroso@gmail.com>
2012-03-19 14:27:31 +0100

    better version

--- analisys/histogram/backproject.py
+++ analisys/histogram/backproject.py
@@ -2,7 +2,8 @@ import cv2;
 import numpy as np;
 import sys;
 
-
+def dummy(val):
+	pass;
 
 if __name__ == "__main__":
 	video = True;
@@ -10,6 +11,8 @@ if __name__ == "__main__":
 	soften = True;
 	original = None;
 	img = None;
+	rectSize = 30;
+	rectangle = True;
 	videoSource = 0;
 	
 	print """Backprojection test.
@@ -42,7 +45,14 @@ if __name__ == "__main__":
 		original = np.copy(img);
 		
 	cv2.namedWindow('backprojection');
-
+	cv2.namedWindow('config');
+	
+		
+	cv2.createTrackbar('rectSize', 'config',10,100, dummy);
+	cv2.createTrackbar('histogram threshold', 'config',120,255, dummy);
+	
+	
+	
 	key = -1;
 	firstHistogram = True;
 	while True:				
@@ -56,44 +66,53 @@ if __name__ == "__main__":
 				print "Soften: "+str(soften);
 		else:
 			img = np.copy(original);
-				
+		
+		
+		histogramThreshold = cv2.getTrackbarPos("histogram threshold","config");
+		rectSize = cv2.getTrackbarPos("rectSize","config");
+			
+		centerX = img.shape[1]/2;
+		centerY = img.shape[0]/2;	
 		
 		imgH,imgS,imgV = cv2.split(cv2.cvtColor( img , cv2.cv.CV_RGB2HSV));
-
-		# Creating x coordenates
-		bins = np.arange(256).reshape(256,1);
-		
-		# Colors just for drawing histogram lines.
-		# Same are used for hsv histograms
-		color = [ (255,0,0),(0,255,0),(0,0,255) ];		
 
 
 		if (firstHistogram or key == 104):
 			firstHistogram = False;
-			hist_item = cv2.calcHist([imgH],[0],None,[256],[0,255]);
+			histItem = cv2.calcHist([imgH[centerY-rectSize:centerY+rectSize,centerX-rectSize:centerX+rectSize]],[0],None,[256],[0,255]);
+			histItemOld = histItem.copy();
 			print "Took histogram";
 		
-		img = np.flipud(img);
-		histCurrent = cv2.calcHist([imgH],[0],None,[256],[0,255]);
-		# Normalize histogram from 0 to 255
-		cv2.normalize(histCurrent ,histCurrent ,0,255,cv2.NORM_MINMAX);
-		# Round and convert to integers
-		histCurrentNormalized=np.int32(np.around(histCurrent));
-		# Stack the points x coordenates with y values
-		currentHistogramPoints = np.column_stack((np.arange(256).reshape(256,1),histCurrentNormalized));
-		# Draw the points with their color
-		cv2.polylines(img,np.array([currentHistogramPoints],np.int32),False,(255,255,255),1);	
-		img = np.flipud(img);
+		
+		retval,histItemThresh = cv2.threshold(histItem, max(histItem)*histogramThreshold/255, max(histItem), cv2.THRESH_TOZERO);
+		
+		if False:
+			# Draw the hue histogram on the original image
+			img = np.flipud(img);
+			histCurrent = cv2.calcHist([imgH],[0],None,[256],[0,255]);
+			cv2.normalize(histCurrent ,histCurrent ,0,255,cv2.NORM_MINMAX);
+			histCurrentNormalized=np.int32(np.around(histCurrent));
+			currentHistogramPoints = np.column_stack((np.arange(256).reshape(256,1),histCurrentNormalized));
+			cv2.polylines(img,np.array([currentHistogramPoints],np.int32),False,(255,255,255),1);	
+			img = np.flipud(img);
 			
-		bp = cv2.calcBackProject([imgH], [0], hist_item, [0,255], 1);
+		bp = cv2.calcBackProject([imgH], [0], histItemThresh, [0,255], 1);
 		thresh_ret,bp_thresh = cv2.threshold(bp,120, 255,cv2.THRESH_BINARY);
 		
+		# Draw rectangle for object fitting
+		if rectangle:
+			cv2.rectangle(img, 
+						(centerX-rectSize,centerY-rectSize),
+						(centerX+rectSize,centerY+rectSize), 
+						(255,255,255));
 		
 		cv2.imshow("backprojection",bp);
 		cv2.imshow("backprojection threshold",bp_thresh);
-		cv2.imshow("hue",imgH);
 		cv2.imshow("original",img);
 		
 		key = cv2.waitKey(5);
+		if (key == 114):
+			rectangle = not rectangle;
+			
 		if (key == 27 or key == 113):
 			break;
