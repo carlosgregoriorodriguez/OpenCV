c61990bfedbe830526acadaf4de93386a2441bb8
Willie Monge <willie9monge@gmail.com>
2012-04-19 10:51:58 +0200

    Improved houghCircles

--- features/basic/houghCircles.py
+++ features/basic/houghCircles.py
@@ -15,33 +15,43 @@ if __name__ == '__main__':
 
 	img = cv2.imread("../img/stop.jpg",0);  #0 for grayscale
 
-	cv2.imshow("ORIGINAL",img);
-
 	#create trackbar window
 	cv2.namedWindow("Trackbars");
 
 	#create trackbars
 	cv2.createTrackbar("dp","Trackbars",1,2,dummy2);
-	cv2.createTrackbar("minDist","Trackbars",2,100,dummy);
-	cv2.createTrackbar("minRadius","Trackbars",0,50,dummy);  #OPTIONAL
-	cv2.createTrackbar("maxRadius","Trackbars",0,200,dummy); #OPTIONAL
+	cv2.createTrackbar("minDist","Trackbars",2,400,dummy);
+	cv2.createTrackbar("minRadius","Trackbars",0,200,dummy);  #OPTIONAL
+	cv2.createTrackbar("maxRadius","Trackbars",50,400,dummy); #OPTIONAL
 
 
 	mthd = cv.CV_HOUGH_GRADIENT   #only method implemented for HoughCircles, CV_HOUGH_GRADIENT = 3
 
 	while True:
 
+		hghcrcl = [];
+		hghcrcl = cv2.HoughCircles(img,method=mthd,dp=cv2.getTrackbarPos("dp","Trackbars"),
+			minDist=cv2.getTrackbarPos("minDist","Trackbars"));
+
 		#if the OPTIONAL trackbars are at 0
-		if cv2.getTrackbarPos("minRadius","Trackbars")==0 or cv2.getTrackbarPos("maxRadius","Trackbars")==0: #if the trackbars are at 0
-			result = cv2.HoughCircles(img,method=mthd,
-				dp=cv2.getTrackbarPos("dp","Trackbars"),minDist=cv2.getTrackbarPos("minDist","Trackbars"));
+		# if cv2.getTrackbarPos("minRadius","Trackbars")==0 or cv2.getTrackbarPos("maxRadius","Trackbars")==0: #if the trackbars are at 0
+		# 	result = cv2.HoughCircles(img,method=mthd,
+		# 		dp=cv2.getTrackbarPos("dp","Trackbars"),minDist=cv2.getTrackbarPos("minDist","Trackbars"));
 
-		else :
-			result = cv2.HoughCircles(img,method=mthd,dp=cv2.getTrackbarPos("dp","Trackbars"),
-				minDist=cv2.getTrackbarPos("minDist","Trackbars"),minRadius=cv2.getTrackbarPos("minRadius","Trackbars"),
-				maxRadius=cv2.getTrackbarPos("maxRadius","Trackbars"));
+		# else :
+		# 	result = cv2.HoughCircles(img,method=mthd,dp=cv2.getTrackbarPos("dp","Trackbars"),
+		# 		minDist=cv2.getTrackbarPos("minDist","Trackbars"),minRadius=cv2.getTrackbarPos("minRadius","Trackbars"),
+		# 		maxRadius=cv2.getTrackbarPos("maxRadius","Trackbars"));
 
-		#print(result);
+		print(hghcrcl);
+
+		for circle in hghcrcl:
+			posX = circle[0];
+			posY = circle[1];
+			radius = circle[2];
+			cv2.circle(img, (posX, posY), radius, (255, 0, 0));
+
+		cv2.imshow("HoughCircles",img);
 
 		if (cv2.waitKey(5)!=-1):
 			break
