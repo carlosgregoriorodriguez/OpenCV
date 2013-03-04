1f774af3326da1e5ef171481fbbc97455c61efe3
gemavb <gemavb22@gmail.com>
2013-03-04 20:45:07 +0100

    search the most similar image by color

--- butterflies/searchColor.py
+++ butterflies/searchColor.py
@@ -163,17 +163,92 @@ if __name__ == "__main__":
 '''
 
 
+    chi = True
+
     cv2.namedWindow('config2')
-    cv2.createTrackbar('eps1','config2',10,100,dummy)
-    cv2.createTrackbar('eps2','config2',80,100,dummy)
-    cv2.createTrackbar('eps3','config2',80,100,dummy)
+    cv2.createTrackbar('CORR---epsB','config2',10,20,dummy)
+    cv2.createTrackbar('CORR---epsG','config2',10,20,dummy)
+    cv2.createTrackbar('CORR---epsR','config2',10,20,dummy)
+    cv2.createTrackbar('CHI---epsB','config2',100,100,dummy)
+    cv2.createTrackbar('CHI---epsG','config2',100,100,dummy)
+    cv2.createTrackbar('CHI---epsR','config2',100,100,dummy)
    
     pimg = compare_images[0][1]
+    histB = cv2.calcHist([cv2.split(pimg)[0]],[0],compare_images[0][0],[256],[0,255])
+    cv2.normalize(histB,histB,0,1,cv2.NORM_MINMAX)
+    histG = cv2.calcHist([cv2.split(pimg)[1]],[0],compare_images[0][0],[256],[0,255])
+    cv2.normalize(histG,histG,0,1,cv2.NORM_MINMAX)
+    histR = cv2.calcHist([cv2.split(pimg)[2]],[0],compare_images[0][0],[256],[0,255])
+    cv2.normalize(histR,histR,0,1,cv2.NORM_MINMAX)
 
-    #addimg = cv2.add(pimg,pimg,pimg,mask = compare_images[0][0])
-    #cv2.imshow('m',addimg)
-    print compare_images[0][0].shape,compare_images[0][0].dtype, cv2.split(pimg)[0].shape,cv2.split(pimg)[0].dtype
-    #cv2.waitKey(0)
-    hist = cv2.calcHist(cv2.split(pimg)[0],[0],compare_images[0][0],[256],[0,255])
-    #print hist.shape
+    
+   
+    def update2():
+        global j
+        
+        
+        imToShow = []
+        for im in compare_images:
+            img = im[1]
+            histB1 = cv2.calcHist([cv2.split(img)[0]],[0],im[0],[256],[0,255])
+            cv2.normalize(histB1,histB1,0,1,cv2.NORM_MINMAX)
+            histG1 = cv2.calcHist([cv2.split(img)[1]],[0],im[0],[256],[0,255])
+            cv2.normalize(histG1,histG1,0,1,cv2.NORM_MINMAX)
+            histR1 = cv2.calcHist([cv2.split(img)[2]],[0],im[0],[256],[0,255])
+            cv2.normalize(histR1,histR1,0,1,cv2.NORM_MINMAX)
+            
+
+            if not chi:
+                eps = [cv2.getTrackbarPos('CORR---epsB','config2')/10.0-1,cv2.getTrackbarPos('CORR---epsG','config2')/10.0-1,cv2.getTrackbarPos('CORR---epsR','config2')/10.0-1]
+                v1 = cv2.compareHist(histB, histB1, cv2.cv.CV_COMP_CORREL)
+                v2 = cv2.compareHist(histG, histG1, cv2.cv.CV_COMP_CORREL)
+                v3 = cv2.compareHist(histR, histR1, cv2.cv.CV_COMP_CORREL)
+
+                v = [v1, v2, v3]
+
+                for i in range(len(v)):
+                    if v[i]< eps[i]:
+                        break
+                    elif i == len(v)-1:
+                        imToShow = imToShow+[img]
+
+            else :
+                eps = [(cv2.getTrackbarPos('CHI---epsB','config2')/10.0),(cv2.getTrackbarPos('CHI---epsG','config2')/10.0),(cv2.getTrackbarPos('CHI---epsR','config2')/10.0)]
+                v1 = cv2.compareHist(histB, histB1, cv2.cv.CV_COMP_CHISQR)
+                v2 = cv2.compareHist(histG, histG1, cv2.cv.CV_COMP_CHISQR)
+                v3 = cv2.compareHist(histR, histR1, cv2.cv.CV_COMP_CHISQR)
+
+                v = [v1, v2, v3]
+            
+                for i in range(len(v)):
+                    if v[i]> eps[i]:
+                        break
+                    elif i == len(v)-1:
+                        imToShow = imToShow+[img]
+
+
+        j = 1
+        for im in imToShow:
+            cv2.imshow('im_'+str(j),im)
+            j = j+1
+            
+
+    update2()
+    while True:
+        cv2.imshow('image',pimg)
+        key = cv2.waitKey(5)
+        if key == ord('s'):
+            for k in range(j):
+                cv2.destroyWindow('im_'+str(k))
+            cv2.imshow('image',pimg)
+            update2()
+        if key == ord('c'):
+            chi = not chi
+            if chi:
+                print 'chi square'
+            else:
+                print 'correlation'
+        if key == ord('q'):
+            break
+    
    
