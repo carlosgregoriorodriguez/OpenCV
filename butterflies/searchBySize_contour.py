90889ea45470a194e58b6e1a9a91fc6969f3b1a4
gemavb <gemavb22@gmail.com>
2013-04-22 20:24:02 +0200

    new changes added for contour detection and comparison with matchShapes

--- butterflies/searchBySize_contour.py
+++ butterflies/searchBySize_contour.py
@@ -7,7 +7,7 @@ import sys
 import numpy as np
 import calcBinaryImage2
 
-help_message = '''USAGE: searchBySize.py [<image for compare>,<image>,...]
+help_message = '''USAGE: searchBySize_contour.py [<image for compare>,<image>,...]
 
 Keys:
 
@@ -24,7 +24,7 @@ def onShowImages(pos=None):
         cv2.destroyWindow('im_mask_'+str(im[2]))
         cv2.destroyWindow(str(im[2]))
     # compare with new data
-    compare(nu, compare_images, cv2.getTrackbarPos('show all input images', 'config2'), cv2.getTrackbarPos('show all selected masks','config2'))
+    compare(buttContours, compare_images,cv2.getTrackbarPos('show all input images', 'config2'), cv2.getTrackbarPos('show all selected masks','config2') )
 
 def onMaskImages(pos):
     # shows all mask of compare_images
@@ -47,8 +47,85 @@ def onMaskImages(pos):
 def dummy(pos):
     a = pos
 
+# finds butterfly's contour
+def findButtContour(mask_img,real_img):
+    
+    # calculates butterfly area with moments (the number of "white" pixels)
+    moments = cv2.moments(mask_img, True)
+    area = moments['m00']
+
+    #img_erode = cv2.erode(mask_img, kernel=None, iterations=3)
+
+    # calculates contours
+    img_canny = cv2.Canny(mask_img, 45, 134)
+    contours, hier = cv2.findContours(img_canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = [cv2.approxPolyDP(contour, 1, True) for contour in contours]
+
+    # for each contour, checks if is the butterfly's contour
+    for i in range(len(contours)):
+        butt_contour = [np.array(0),None]
+        img_copy = real_img.copy()
+        cont_area = cv2.contourArea(contours[i])
+        cv2.drawContours(img_copy,contours,i, (0,255,255),1,cv2.CV_AA)
+        
+        # if cont_area is similar to area then save this contour and stop looking
+        if cont_area > area-area/2:
+            butt_contour = [contours[i],i]
+            print 'real_area: ',area,'  cont_area: ',cont_area
+            break
+        else:
+            # calculates minEnclosingCircle and boundingRect, keeps the minimum of these and compares this area (area_comp) with the butterfly's contour area
+            center, radius = cv2.minEnclosingCircle(contours[i])
+            rect = cv2.boundingRect(contours[i])
+            area_circle = radius*radius*np.pi
+            area_rect = rect[2]*rect[3]
+            area_comp = min(area_circle, area_rect)
+            print 'area_circle: ',area_circle,'  area_rect: ', area_rect,'  real_area: ', area, '  cont_area: ',cont_area
+            # if area_comp is similar to area then save this contour and stop looking
+            if area_comp<area+area and area_comp>area-area/2:
+                butt_contour = [contours[i],i]
+                break
+    print 'contour... ',butt_contour[1]
+    if butt_contour[1] != None:
+        cv2.drawContours(real_img,contours,butt_contour[1], (255,0,255),1,cv2.CV_AA)
+    cv2.imshow('final',real_img)
+    cv2.waitKey(0)
+    cv2.destroyWindow('final')
+    return butt_contour
+
+def compare(buttContours, compare_images, showAllImages=0, showSelectMask=0):
+    imSelect = []
+    principalContour = buttContours[0][0]
+    for i in range(len(buttContours)):
+
+        # if showAllImages!=0 then want show all compare_images
+        if showAllImages==1:# shows real image
+            cv2.imshow(str(compare_images[i][2]),compare_images[i][1])
+
+        # compare contours
+        if buttContours[i][1]==None:
+            print '¡¡¡¡¡¡¡¡¡¡¡¡¡¡ not found this contour !!!!!!!!!!!'
+        else:
+            comp1 = cv2.matchShapes(principalContour, buttContours[i][0], cv2.cv.CV_CONTOURS_MATCH_I1,0)
+            comp2 = cv2.matchShapes(principalContour, buttContours[i][0], cv2.cv.CV_CONTOURS_MATCH_I2,0)
+            comp3 = cv2.matchShapes(principalContour, buttContours[i][0], cv2.cv.CV_CONTOURS_MATCH_I3,0)
+
+        #print cv2.getTrackbarPos('eps-I1','config2')/100.0, cv2.getTrackbarPos('eps-I2','config2')/100.0, cv2.getTrackbarPos('eps-I3','config2')/100.0
+
+        if comp1<cv2.getTrackbarPos('eps-I1','config2')/100.0 and comp2<cv2.getTrackbarPos('eps-I2','config2')/100.0 and comp3<cv2.getTrackbarPos('eps-I3','config2')/100.0:
+            copy = compare_images[i][1].copy()
+            cv2.putText(copy,'['+str(comp1)+', '+str(comp2)+', '+str(comp3)+']',(50,copy.shape[0]-25),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0))
+            imSelect = imSelect + [[compare_images[i][0],copy,compare_images[i][2]]]
+
+    # shows selected images
+    for img in imSelect:
+        cv2.imshow('selected_'+str(img[2]),img[1])
+        if showSelectMask == 1:# show selected image mask
+            cv2.imshow('im_mask_'+str(img[2]),img[0])
+
+
 def compareBySize(images_name):
-    global H,compare_images,nu
+    global buttContours,compare_images
     print help_message
 
     ######### STEP 1 #################
@@ -80,58 +157,52 @@ def compareBySize(images_name):
 
     # creates config window with its trackbars
     cv2.namedWindow('config2')
-    cv2.createTrackbar('eps_cont_area','config2',1500,5000,dummy)
-    cv2.createTrackbar('show all input images', 'config2',0,2,onShowImages)
+    cv2.createTrackbar('eps_cont_area','config2',10000,20000,dummy)
+    cv2.createTrackbar('eps-I1','config2',10,100,dummy)
+    cv2.createTrackbar('eps-I2','config2',100,500,dummy)
+    cv2.createTrackbar('eps-I3','config2',10,100,dummy)
+    cv2.createTrackbar('show all input images', 'config2',0,1,onShowImages)
     cv2.createTrackbar('show all masks','config2', 0,2,onMaskImages)
     cv2.createTrackbar('show all selected masks','config2', 0,1,onShowImages)
 
+    '''
     # selects main image for comparison and calculates its moments
     mask_img = compare_images[0][0]
+    real_img = compare_images[0][1]
     print type(mask_img), mask_img.dtype
     #mask_img = cv2.imread('mask_BMC-1968_D.jpg',-1)
     #print type(mask_img), mask_img.dtype
-    
-    real_img = compare_images[0][1]
-    moments = cv2.moments(mask_img, True)
-    area = moments['m00']
+    '''
 
-    img_erode = cv2.erode(mask_img, kernel=None, iterations=3)
-    img_canny = cv2.Canny(img_erode, 45, 134)
-    cv2.imshow("canny", img_canny)
-
-    contours, hier = cv2.findContours(img_canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = [cv2.approxPolyDP(contour, 1, True) for contour in contours]
-    print len(contours)
-    cv2.drawContours(real_img,contours,-1,(255,0,0),1, cv2.CV_AA)
-    for i in range(len(contours)):
-        #print contours[i]
-        img_copy = real_img.copy()
-        cv2.drawContours(img_copy,contours,i, (0,0,255),1,cv2.CV_AA)
-        cont_area = cv2.contourArea(contours[i])
-        print 'area... ',cont_area
-        cv2.imshow('im',img_copy)
-        cv2.waitKey(0)
+    buttContours = []
+    for img in compare_images:
+        mask_img = img[0]
+        real_img = img[1]
+        buttContours = buttContours + [np.array(findButtContour(mask_img,real_img))]
         
-        if cont_area > area-cv2.getTrackbarPos('eps_cont_area','config2'):
-            butt_contour = contours[i]
+    compare(buttContours, compare_images)
+        
+    '''
     print "comparando contornos 2 y 3", cv2.matchShapes(contours[2], contours[3], cv2.cv.CV_CONTOURS_MATCH_I1,0)
     print "comparando contornos 2 y 3", cv2.matchShapes(contours[2], contours[3], cv2.cv.CV_CONTOURS_MATCH_I2,0)
     print "comparando contornos 2 y 3", cv2.matchShapes(contours[2], contours[3], cv2.cv.CV_CONTOURS_MATCH_I3,0)
     print "comparando contornos 1 y 3", cv2.matchShapes(contours[1], contours[3], cv2.cv.CV_CONTOURS_MATCH_I1,0)
     print "comparando contornos 1 y 3", cv2.matchShapes(contours[1], contours[3], cv2.cv.CV_CONTOURS_MATCH_I2,0)
     print "comparando contornos", cv2.matchShapes(contours[1], contours[3], cv2.cv.CV_CONTOURS_MATCH_I3,0)
-        
+    '''    
    
     while True:
-        cv2.imshow('image',img)
+        cv2.imshow(str(compare_images[0][2]),compare_images[0][1])
         key = cv2.waitKey(5)
         # removes old windows and calculates the most similar images with the new values
         if key == ord('s'):
+            print 's'
             for im in compare_images:
-                cv2.destroyWindow('im_'+str(im[2]))
-                cv2.destroyWindow('im_mask_'+str(im[2]))
                 cv2.destroyWindow('selected_'+str(im[2]))
-            cv2.imshow('image',img)
+                cv2.destroyWindow('im_mask_'+str(im[2]))
+                cv2.destroyWindow(str(im[2]))
+            cv2.imshow(str(compare_images[0][2]),compare_images[0][1])
+            compare(buttContours, compare_images,cv2.getTrackbarPos('show all input images', 'config2'), cv2.getTrackbarPos('show all selected masks','config2'))
         # EXIT
         if key == ord('q'):
             return
