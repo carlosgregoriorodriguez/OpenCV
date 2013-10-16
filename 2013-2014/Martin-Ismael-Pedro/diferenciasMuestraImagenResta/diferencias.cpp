aeefb3893bd428d643bfb36de982b6c0f1b0239a
ismael <ismaelfuentes90@gmail.com>
2013-10-16 23:51:27 +0200

    Carpetas y codigo de resta
    He creado carpetas para cada ejemplo, para asi no perder lo anterior y
    he subido un ejemplo que hace la diferencia entre dos imagenes y muestra
    la imagen resultante

---
+++ 2013-2014/Martin-Ismael-Pedro/diferenciasMuestraImagenResta/diferencias.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include <opencv2/core/core.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+void dummy(int, void *);
+
+int main( int argc, char** argv )
+{
+	//Vars
+	Mat image1,image2;
+
+	//Read both images
+	image1 = imread("image1.jpg", CV_LOAD_IMAGE_COLOR);
+	image2 = imread("image2.jpg", CV_LOAD_IMAGE_COLOR);
+
+	// Check for invalid input
+	if(!image1.data || !image2.data )
+	{
+		cout <<  "Could not open or find the image" << std::endl ;
+		return -1;
+	}
+
+	//Get the diference
+	MatExpr c = abs(image1-image2);
+
+	//Create the window
+	namedWindow("Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
+
+	//Show the image
+	imshow("Display window", c);
+
+	//Wait
+	waitKey(0); 
+}
