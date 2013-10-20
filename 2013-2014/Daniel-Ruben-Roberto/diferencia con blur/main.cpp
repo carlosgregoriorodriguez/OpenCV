32c98b3ece7e89d3f8a73c459e528a9005857936
aldoc <tekawer@hotmail.es>
2013-10-20 23:16:31 +0200

    añadido blur al código de las diferencias
    Al código original de hacer la resta absoluta, y sacar los bordes, se le
    añade la posibilidad de hacer blur de esta resta absoluta, y luego sacar
    los bordes

---
+++ 2013-2014/Daniel-Ruben-Roberto/diferencia con blur/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: daniel
+ *
+ * Created on 19 de octubre de 2013, 11:59
+ */
+
+#include <cstdlib>
+#include "resta.h"
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    String path1= "images/image1.jpg";
+    String path2= "images/image2.jpg";
+
+	String path3 = "images/beach1.jpg";
+	String path4 = "images/beach2.jpg";
+
+    int i;
+    cout << "i (para el suavizado):";
+    cin >> i;
+    calculaDiferencias(path3, path4, i);
+    return 0;
+}
+
