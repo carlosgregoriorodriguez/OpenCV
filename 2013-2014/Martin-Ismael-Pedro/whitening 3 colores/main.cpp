0c6ffb3d3999e459e4568ba20ffbf20c968374ae
pedropabloperea <pedroper@estumail.ucm.es>
2013-11-02 15:59:22 +0100

    Update main.cpp
    whitening

--- 2013-2014/Martin-Ismael-Pedro/whitening 3 colores/main.cpp
+++ 2013-2014/Martin-Ismael-Pedro/whitening 3 colores/main.cpp
@@ -18,26 +18,11 @@ using namespace std;
 int main(){
     Mat src,dst,dst1,dst2 ;
     src=imread("/Users/pedrete_142/Documents/proyectoprimeroopencv/multimedia/images/beach1.jpg" );
-    //cvtColor(src, dst, CV_BGR2GRAY);
+    
     Whitening wh(src);
-    /*src=imread("/Users/pedrete_142/Documents/proyectoprimeroopencv/multimedia/images/botin1.jpg");
-    cvtColor(src, dst, CV_8U);
-    
-    //Mat s=src.col(0);
 
-    namedWindow("imagen normalizada");
-    imshow("imagen normalizada", dst);
-    
-    */
-    //double media,varianza;
-    //media=wh.media();
-    //varianza=wh.varianza();
-    //cout<<media;
-    //wh.mostrar();
     wh.whitening();
-    //printf("Resultado %f",media);
-    //printf("Varianza %f",varianza);
-    //wh.mostrar();
+   
     waitKey(0);
     
 }
