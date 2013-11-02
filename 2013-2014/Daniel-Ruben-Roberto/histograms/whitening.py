cb484359c749818474030ec29a158007041f6ef5
dansirak <dansirak@gmail.com>
2013-11-02 16:28:36 +0100

    Whitening "completo"

--- 2013-2014/Daniel-Ruben-Roberto/histograms/whitening.py
+++ 2013-2014/Daniel-Ruben-Roberto/histograms/whitening.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 
 #Inicializamos variables
-img = cv2.imread("../../img/beach1.jpg", cv2.CV_LOAD_IMAGE_GRAYSCALE)
+img = cv2.imread("/home/daniel/foto.png", cv2.CV_LOAD_IMAGE_GRAYSCALE)
 cv2.imshow("original", img)
 
 I, J = img.shape
@@ -21,7 +21,7 @@ media = media/(I*J)
 #Calculamos varianza
 for i in range(I):
     for j in range(J):
-        var += (abs(img[i, j] - media))**2
+        var += (img[i, j] - media)**2
 var = var/(I*J)
 
 #Blanqueamos
@@ -29,6 +29,6 @@ vis = np.zeros((I, J), np.float32)
 dst = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
 for i in range(I):
     for j in range(J):
-        dst[i, j] = (abs(img[i, j] - media))/(var**0.5)
+        dst[i, j] = (img[i, j] - media)/(var**0.5)
 cv2.imshow("resultado", dst)
 cv2.waitKey(0)
