4c8b8708edd2e046b28725c7049dc1bd0c9984a2
aldoc <tekawer@hotmail.es>
2014-03-17 23:16:12 +0100

    k means con cáculo de áreas y coloreado

---
+++ 2013-2014/Daniel-Ruben-Roberto/Kmeans con cáculo de áreas y colores/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: Daniel
+ *
+ * Created on 15 de diciembre de 2013, 18:47
+ */
+
+#include <cstdlib>
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "heridas.h";
+
+
+using namespace std;  
+using namespace cv;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //preparaImagen("img/img1.jpg");
+    aplicaKmeans("img/crop.jpg");
+}
+
