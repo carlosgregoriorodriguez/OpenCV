68a7d316e174d7bf706bc03055100554c2d7ff0d
mimadrid <mimadrid@ucm.es>
2015-04-15 19:58:09 +0200

    Añadir pruebas de procesamiento de archivos

--- 2014-2015/OpenCV/args_processor.py
+++ 2014-2015/OpenCV/args_processor.py
@@ -5,6 +5,35 @@ import os
 import glob
 import cv2
 import shutil
+# shutil.rmtree(carpeta_procesada)
+
+
+def crear_carpeta(carpeta_procesada):
+    if not os.path.exists(carpeta_procesada):
+        os.mkdir(carpeta_procesada)
+
+def aplicar_algoritmo(img_name, carpeta_procesada):
+    image = cv2.imread(img_name)
+    cv2.imshow(img_name, image)
+    # cv2.imwrite(os.path.splitext(img_name)[0] + '_PROCESADA' + os.path.splitext(img_name)[1], image)
+    os.chdir(carpeta_procesada)
+    cv2.imwrite(img_name, image)
+    os.chdir('..')
+
+
+def procesar_archivo(img_name, carpeta_procesada):
+    crear_carpeta(carpeta_procesada)
+    aplicar_algoritmo(img_name, carpeta_procesada)
+
+
+def procesar_carpeta(carpeta_procesada):
+    img_names = []
+    img_types = ('*.bmp', '*.tif', '*.png')
+    crear_carpeta(carpeta_procesada)
+    for img_type in img_types:
+            img_names.extend(glob.glob(img_type))
+            for img_name in img_names:
+                aplicar_algoritmo(img_name, carpeta_procesada)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Programa para la medición automática de la coroides')
@@ -16,28 +45,18 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    img_types = ('*.bmp', '*.tif', '*.png')
-    img_names = []
-
-    carpeta_procesadas = 'PROCESADAS'
+    carpeta_procesada = 'PROCESADAS'
 
     if args.pasos:
         print 'opción pasos'
     if args.archivos:
         print 'opción archivos'
+        for archivo in args.argumentos:
+            procesar_archivo(archivo, carpeta_procesada)
+
     if args.carpetas:
         print 'opción carpetas'
-        for x in args.argumentos:
-            os.chdir(x)
-            if os.path.exists(carpeta_procesadas):
-                shutil.rmtree(carpeta_procesadas)
-            os.mkdir(carpeta_procesadas)
-            for img_type in img_types:
-                img_names.extend(glob.glob(img_type))
-                for img_name in img_names:
-                    image = cv2.imread(img_name)
-                    cv2.imshow(img_name, image)
-                    # cv2.imwrite(os.path.splitext(img_name)[0] + '_PROCESADA' + os.path.splitext(img_name)[1], image)
-                    os.chdir(carpeta_procesadas)
-                    cv2.imwrite(img_name, image)
-                    os.chdir('..')
+        for carpeta in args.argumentos:
+            os.chdir(carpeta)
+            procesar_carpeta(carpeta_procesada)
+            os.chdir('..')
