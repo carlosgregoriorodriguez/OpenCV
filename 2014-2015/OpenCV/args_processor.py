2a9418a02699f24b03afceff1bfde613086424dc
mimadrid <mimadrid@ucm.es>
2015-04-15 18:44:17 +0200

    Añadir prueba de procesar carpeta de imágenes

--- 2014-2015/OpenCV/args_processor.py
+++ 2014-2015/OpenCV/args_processor.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import os
+import glob
+import cv2
+import shutil
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Programa para la medición automática de la coroides')
@@ -12,11 +16,28 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    img_types = ('*.bmp', '*.tif', '*.png')
+    img_names = []
+
+    carpeta_procesadas = 'PROCESADAS'
+
     if args.pasos:
-        print 'pasos'
+        print 'opción pasos'
     if args.archivos:
-        print 'archivos'
+        print 'opción archivos'
     if args.carpetas:
-        print 'carpetas'
-    for x in args.argumentos:
-        print x
+        print 'opción carpetas'
+        for x in args.argumentos:
+            os.chdir(x)
+            if os.path.exists(carpeta_procesadas):
+                shutil.rmtree(carpeta_procesadas)
+            os.mkdir(carpeta_procesadas)
+            for img_type in img_types:
+                img_names.extend(glob.glob(img_type))
+                for img_name in img_names:
+                    image = cv2.imread(img_name)
+                    cv2.imshow(img_name, image)
+                    # cv2.imwrite(os.path.splitext(img_name)[0] + '_PROCESADA' + os.path.splitext(img_name)[1], image)
+                    os.chdir(carpeta_procesadas)
+                    cv2.imwrite(img_name, image)
+                    os.chdir('..')
