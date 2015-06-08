c599aeb801f5682ebafb321719f95fc0dd10fb91
mimadrid <mimadrid@ucm.es>
2015-06-08 21:03:13 +0200

    Parecer detección poros a la de oftalmólogos

--- 2014-2015/OpenCV/algorithm_blobs_SimpleCV.py
+++ 2014-2015/OpenCV/algorithm_blobs_SimpleCV.py
@@ -9,15 +9,13 @@ if __name__ == '__main__':
     inv = img.invert()
     blobs = inv.findBlobs(maxsize=150)
     for blob in blobs:
-
         # print blob.coordinates()
         # print blob.area()
+        blob.draw()
+        # img.dl().circle(blob.coordinates(), 10, SimpleCV.Color.GREEN, filled=False)
+        # img.dl().circle(blob.coordinates(), 2, SimpleCV.Color.RED, filled=True)
 
-        img.dl().circle(blob.coordinates(), 10, SimpleCV.Color.GREEN, filled=False)
-        img.dl().circle(blob.coordinates(), 2, SimpleCV.Color.RED, filled=True)
-
-    # blobs.show(color=SimpleCV.Color.GREEN, width=1)
-    # blobs.draw(color=SimpleCV.Color.GREEN, width=1)
-    
-    img.show()
-    time.sleep(3)
+   # blobs.show(color=SimpleCV.Color.GREEN)
+    # blobs.draw(color=SimpleCV.Color.GREEN, width=5)
+    img.addDrawingLayer(inv.dl())
+    img.save("poros papila procesada.bmp")
