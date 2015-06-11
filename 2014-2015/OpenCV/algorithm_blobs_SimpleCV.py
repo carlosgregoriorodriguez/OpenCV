303a877fc756694e15bf30a4b869cf6351815674
mimadrid <mimadrid@ucm.es>
2015-06-11 22:25:05 +0200

    Retoques en el código de los poros

--- 2014-2015/OpenCV/algorithm_blobs_SimpleCV.py
+++ 2014-2015/OpenCV/algorithm_blobs_SimpleCV.py
@@ -31,22 +31,32 @@
 # IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE
 # SOFTWARE.
 
-import SimpleCV
+import sys
+import os
 
+import SimpleCV
 
 if __name__ == '__main__':
 
-    img = SimpleCV.Image("poros papila.bmp")
-    inv = img.invert()
-    blobs = inv.findBlobs(maxsize=150)
-    for blob in blobs:
-        # print blob.coordinates()
-        # print blob.area()
-        blob.draw()
-        # img.dl().circle(blob.coordinates(), 10, SimpleCV.Color.GREEN, filled=False)
-        # img.dl().circle(blob.coordinates(), 2, SimpleCV.Color.RED, filled=True)
+    if len(sys.argv) != 2:
+        sys.stderr.write(
+            """usage: """ + os.path.basename(sys.argv[0]) + """ image
+        """ + os.path.basename(sys.argv[0]) + """: error: one image is required
+        """)
+    else:
+        img_file = sys.argv[1]
+        img = SimpleCV.Image(img_file)
+        inv = img.invert()
+        blobs = inv.findBlobs(maxsize=150)
+        for blob in blobs:
+            # print blob.coordinates()
+            # print blob.area()
+            blob.draw()
+            # img.dl().circle(blob.coordinates(), 10, SimpleCV.Color.GREEN, filled=False)
+            # img.dl().circle(blob.coordinates(), 2, SimpleCV.Color.RED, filled=True)
 
-    # blobs.show(color=SimpleCV.Color.GREEN)
-    # blobs.draw(color=SimpleCV.Color.GREEN, width=5)
-    img.addDrawingLayer(inv.dl())
-    img.save("poros papila procesada.bmp")
+        # blobs.show(color=SimpleCV.Color.GREEN)
+        # blobs.draw(color=SimpleCV.Color.GREEN, width=5)
+        img.addDrawingLayer(inv.dl())
+        processed = os.path.splitext(img_file)[0] + '_procesada' + os.path.splitext(img_file)[1]
+        img.save(processed)
