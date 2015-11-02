39565177427a2e636a431aa3cd8d04ebe36ce20a
Luis Llana <llana@sip.ucm.es>
2015-11-02 07:46:35 +0100

    track_color

--- motion/track_color.py
+++ motion/track_color.py
@@ -4,6 +4,23 @@
 
 import cv2
 import sys
+import numpy as np          
+
+def joinImages (images):
+	w = images[0][0].shape[1];
+	h = images[0][0].shape[0];
+	img = np.zeros((len(images)*h, len(images[0])*w,3), np.uint8);
+	for i in range(len(images)):
+		for j in range(len(images[0])):
+			if (images[i][j] != None):  
+				if(len(images[i][j].shape) == 3):
+					img[i*h:(i+1)*h,j*w:(j+1)*w] = images[i][j];
+				else:
+					for k in range(3):
+						img[i*h:(i+1)*h,j*w:(j+1)*w,k] = images[i][j];
+					
+	return img;
+          
           
 def filter_color(img):
     img = cv2.inRange(img,lowerb=(40,40,120),upperb=(65,65,165))
@@ -11,6 +28,9 @@ def filter_color(img):
     img  = cv2.dilate(img,kernel=None,iterations=5) 
     return img
 
+def filter_high(img, value):
+    img = cv2.inRange(img,lowerb=value,upperb=255)
+    return img
 if __name__ == "__main__":
 
     if (len(sys.argv) > 1):
@@ -22,14 +42,25 @@ if __name__ == "__main__":
     else:
         name = "data/treme.avi"
     
-    name = 1
     camera =  cv2.VideoCapture(name)
     f, img = camera.read()		
     while f:	
         #img = img[:,:,0] 
-	cv2.imshow("video", img)
-        filtered = filter_color(img)
-        cv2.imshow("processed", filtered)
+        imgB, imgG, imgR = cv2.split(img) 
+        cv2.cvtColor(img, cv2.COLOR_RGB2HSV);
+        imgHSV = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        imgH, imgS, imgV = cv2.split(imgHSV) 
+        filteredS = filter_high(imgS,170)
+        filteredV = filter_high(imgV,100)
+        filteredG = filter_high(imgG,70)
+        filteredR = filter_high(imgR,70)
+        filteredB = filter_high(imgB,100)
+        sANDv = cv2.bitwise_and(filteredS,filteredV)
+        gsv = cv2.bitwise_and(sANDv,filteredG)
+        rsv = cv2.bitwise_and(sANDv,filteredR)
+        bsv = cv2.bitwise_and(sANDv,filteredB)
+        res = joinImages([[imgR,imgG,imgB],[gsv, rsv, bsv]])
+        cv2.imshow("",res)
         f, img = camera.read()		
         if (cv2.waitKey (5) != -1):
             break
