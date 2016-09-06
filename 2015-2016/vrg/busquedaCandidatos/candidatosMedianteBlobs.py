a72be4e85bb3e1064b8342f69414e9bfd58a9d94
Víctor RG <veketor@gmail.com>
2016-09-06 12:48:09 +0200

    Busqueda de umbral categorización estrellas

--- 2015-2016/vrg/busquedaCandidatos/candidatosMedianteBlobs.py
+++ 2015-2016/vrg/busquedaCandidatos/candidatosMedianteBlobs.py
@@ -1,7 +1,8 @@
 # Standard imports
 import cv2
 import numpy as np;
- 
+import math
+
 # Read image
 
 def getIntersetObjectList(img, minThreshold = 10, maxThreshold=255):
@@ -18,39 +19,69 @@ def getIntersetObjectList(img, minThreshold = 10, maxThreshold=255):
 
 if __name__ == "__main__":
 	print "Ejecutando getIntersetObjectList sobre una serie de 6 imagenes"
-
+	e =  2.71828
 	im = cv2.imread("miniSetEstrellayGalaxia.png", cv2.IMREAD_GRAYSCALE)
 	keyPoints = getIntersetObjectList(im)
+	index=0
+	flux = np.zeros([len(keyPoints)])
+	print flux
+	for k in keyPoints:
+		flux[index] = im.item(int(k.pt[1]), int(k.pt[0]))
+		print "Punto: "+str(index)+" ("+str(int(k.pt[0]))+", "+str(int(k.pt[1]))+") with size :"+str(k.size)+ "and intensity: "+str(im.item(int(k.pt[1]), int(k.pt[0])))
+		#if index<len(keyPoints)//2:
+		cv2.putText(im,str(index), (int(k.pt[0]),int(k.pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.3, 255)
+		index = index + 1
 	resultado = cv2.drawKeypoints(im, keyPoints, np.array([]), (255,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 	cv2.imshow("Imagen Resultado", resultado)
+	print flux
+	print "Umbral flujo estrellas: "+str((np.mean(flux)-np.amin(flux))/e)
 	cv2.waitKey(0)
-	
+
 	im = cv2.imread("image_3417_1e-LEDA-1852.jpg", cv2.IMREAD_GRAYSCALE)
 	keyPoints = getIntersetObjectList(im)
+	index=0
+	for k in keyPoints:
+		print "Punto: "+str(index)+" ("+str(int(k.pt[0]))+", "+str(int(k.pt[1]))+") with size :"+str(k.size)+ "and intensity: "+str(im.item(int(k.pt[1]), int(k.pt[0])))
+		if index<len(keyPoints)//2:
+			cv2.putText(im,str(index), (int(k.pt[0]),int(k.pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255,255,0))
+		index = index + 1
 	resultado = cv2.drawKeypoints(im, keyPoints, np.array([]), (255,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 	cv2.imshow("Imagen Resultado", resultado)
 	cv2.waitKey(0)
 
 	im = cv2.imread("o-HUBBLE-UV-900.jpg", cv2.IMREAD_GRAYSCALE)
 	keyPoints = getIntersetObjectList(im)
+	index=0
+	for k in keyPoints:
+		print "Punto: "+str(index)+" ("+str(int(k.pt[0]))+", "+str(int(k.pt[1]))+") with size :"+str(k.size)+ "and intensity: "+str(im.item(int(k.pt[1]), int(k.pt[0])))
+		if index<len(keyPoints)//2:
+			cv2.putText(im,str(index), (int(k.pt[0]),int(k.pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255,255,0))
+		index = index + 1
 	resultado = cv2.drawKeypoints(im, keyPoints, np.array([]), (255,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 	cv2.imshow("Imagen Resultado", resultado)
 	cv2.waitKey(0)
 	
-	im = cv2.imread("noReal2.jpg", cv2.IMREAD_GRAYSCALE)
-	keyPoints = getIntersetObjectList(im,190)
-	resultado = cv2.drawKeypoints(im, keyPoints, np.array([]), (255,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-	cv2.imshow("Imagen Resultado", resultado)
-	cv2.waitKey(0)
-	
+	print "##############tutifruti.png#############"
 	im = cv2.imread("tutifruti.png", cv2.IMREAD_GRAYSCALE)
 	keyPoints = getIntersetObjectList(im)
+	index=0
+	for k in keyPoints:
+		print "Punto: "+str(index)+" ("+str(int(k.pt[0]))+", "+str(int(k.pt[1]))+") with size :"+str(k.size)+ "and intensity: "+str(im.item(int(k.pt[1]), int(k.pt[0])))
+		#if index<len(keyPoints)//2:
+		cv2.putText(im,str(index), (int(k.pt[0]),int(k.pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255,255,0))
+		index = index + 1
 	resultado = cv2.drawKeypoints(im, keyPoints, np.array([]), (255,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 	cv2.imshow("Imagen Resultado", resultado)
 	cv2.waitKey(0)
 	
 	im = cv2.imread("tutifrutiDesenfocado.png", cv2.IMREAD_GRAYSCALE)
 	keyPoints = getIntersetObjectList(im)
+	index=0
+	for k in keyPoints:
+		print "Punto: "+str(index)+" ("+str(int(k.pt[0]))+", "+str(int(k.pt[1]))+") with size :"+str(k.size)+ "and intensity: "+str(im.item(int(k.pt[1]), int(k.pt[0])))
+		if index<len(keyPoints)//2:
+			cv2.putText(im,str(index), (int(k.pt[0]),int(k.pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255,255,0))
+		index = index + 1
 	resultado = cv2.drawKeypoints(im, keyPoints, np.array([]), (255,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 	cv2.imshow("Imagen Resultado", resultado)
 	cv2.waitKey(0)
