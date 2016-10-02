9edf770156ebb0434364450e0f8ee109f60331a1
Ubuntu KaksiParadise <ubuntu-paradise@ubuntuparadise-System-Product-Name.(none)>
2016-10-02 17:16:35 +0200

    Editado blobDetection.

--- 2015-2016/Miguel/blobDetection.py
+++ 2015-2016/Miguel/blobDetection.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 from astropy.io import fits
+import sys
 import cv2
 import numpy as np
 
-fitsFile="../examples/Filters/frame-i-002830-6-0398.fits"
-hdulist = fits.open(fitsFile)
-img = hdulist[0].data
+fitsFileI="../examples/Filters/frame-i-004264-4-0259.fits" #m81 http://mirror.sdss3.org/fields/name?name=m81
+fitsFileI2="../examples/Filters/frame-i-003804-6-0084.fits" #m66 http://mirror.sdss3.org/fields/name?name=m66
+fitsFileI3='../examples/Filters/frame-i-004381-2-0120.fits' #m91 http://mirror.sdss3.org/fields/name?name=m91
+fitsFileI4='../examples/Filters/frame-i-002830-6-0398.fits' #m109 http://mirror.sdss3.org/fields/name?name=m109
+fitsFileI5='../examples/Filters/frame-i-003805-2-0023.fits' #m59 http://mirror.sdss3.org/fields/name?name=m59
+fitsFileI6='../examples/Filters/frame-i-008112-2-0074.fits' #m33 http://mirror.sdss3.org/fields/name?name=m33
+fitsFileI7='../examples/Filters/frame-i-007845-2-0104.fits' #m74 http://mirror.sdss3.org/fields/name?name=m74
+fitsFileI8='../examples/Filters/frame-i-003836-4-0084.fits' #m95 http://mirror.sdss3.org/fields/name?name=m95
+dataset={'i':fitsFileI,'i2':fitsFileI2,'i3':fitsFileI3,'i4':fitsFileI4,'i5':fitsFileI5,'i6':fitsFileI6,'i7':fitsFileI7,'i8':fitsFileI8}
+
+hdu_list = fits.open(dataset[sys.argv[1]])
+img = hdu_list[0].data
 
 Min=np.amin(img)
 Max=np.amax(img)
@@ -17,13 +27,14 @@ imgProcessed=cv2.morphologyEx(imgBase, cv2.MORPH_CLOSE, (3,3),iterations=5)
 
 cv2.namedWindow("Base image",cv2.WINDOW_NORMAL)
 cv2.imshow("Base image", imgProcessed)
+cv2.imwrite("imgClose.png",255*imgProcessed)
 cv2.waitKey(0)
-#cv2.imwrite("img4.png",imgProcessed)
+
 
 
 #Código basado en el manual https://www.learnopencv.com/blob-detection-using-opencv-python-c/
 
-im = cv2.imread("img4.png") #Carga de la imagen generada aplicando la operación cierre
+im = cv2.imread("imgClose.png") #Carga de la imagen generada aplicando la operación cierre
 
 #Creación del contenedor de los parámetros
 params = cv2.SimpleBlobDetector_Params()
